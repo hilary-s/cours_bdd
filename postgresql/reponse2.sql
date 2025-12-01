19c2992dd89b2634132056fa24476ed0599315b4
hilary-s <somhilary0103@gmail.com>
2025-12-01 15:17:41 +0000

    modif du fichier reponse2.sql

--- postgresql/reponse2.sql
+++ postgresql/reponse2.sql
@@ -1,6 +1,20 @@
 -- Affiche la liste des clients avec leurs commandes (même ceux qui n’ont pas de commande).
+SELECT c.nom, co.commande_id
+FROM clients c
+RIGHT JOIN commandes co ON c.client_id = co.client_id;
+
 -- Affiche toutes les commandes avec le nom du client et leur statut.
+SELECT c.nom, co.statut, co.commande_id
+FROM clients c
+RIGHT JOIN commandes co ON c.client_id = co.client_id;
+
 -- Affiche les produits commandés par "Alice Dupont".
+SELECT c.nom, p.nom, p.prix
+FROM commandes co
+INNER JOIN clients c ON co.client_id = c.client_id
+INNER JOIN lignes_commandes lc ON co.commande_id = lc.commande_id
+INNER JOIN produits p ON lc.produit_id = p.produit_id
+WHERE c.nom = 'Alice Dupont';
 -- Affiche les clients qui n’ont jamais passé de commande.
 -- Affiche toutes les commandes avec les produits associés et la quantité commandée.
 -- Affiche les clients et les produits qu’ils ont commandés, en utilisant une jointure multiple.
